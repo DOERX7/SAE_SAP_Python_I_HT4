3bbbcbafa4baacbdb8f025c278296c62cf49d1c6
David Omar Enriquez Reyes <davidomarenriquezreyes@gmail.com>
2021-03-12 16:05:09 -0600

    Refinando detalles, comunicacion con el usuario mejorada

--- HT4.py
+++ HT4.py
@@ -11,17 +11,22 @@ tabla_de_frutas_minusculas={
     'pera': 0.85,
     'naranja': 0.7
 }
+def mostrar_tabla_fruas():
+    print('fruta - precio por Kg')
+    for a in tabla_de_frutas:
+        print(a + ' - '+ str(tabla_de_frutas[a]))
 
 def pedir_fruta():
     seguir = True
     while(seguir):
+        mostrar_tabla_fruas()
         nom_fruta = input('Que fruta desea?')
         nom_fruta = nom_fruta.lower()
         if (nom_fruta in tabla_de_frutas_minusculas):
-            cant_fruta = input("Cuantas de estas frutas desea?")
+            cant_fruta = input("Cuantos kilos de esta fruta desea?")
             cant_f = float(cant_fruta)
             total = round(cant_f*tabla_de_frutas_minusculas[nom_fruta],2)
-            print(total)
+            print("Costo total por "+str(cant_f)+"Kg de "+str(nom_fruta)+" = "+str(total))
         else:
             print('Esta fruta no esta disponible :C')
 
@@ -31,17 +36,24 @@ def pedir_fruta():
             return
 
 def tabla_de_multiplicar():
-    num = input("Que numero desea multiplicar?")
-    num = int(num)
-    content = "Tabla del "+str(num)+"\n"
+    num = input("De que numero entre el 1 al 10 desea la tabla de multiplicar?")
+    try:
+        num = int(num)
+        if num<0 or num>10:
+            print('Solo se permiten numeros del 1 al 10')
+            return
+    except:
+        print('Ese no era un numero valido')
+    content = "Tabla del "+str(num)
     i=0
     while i <= 10:
-        content+="\n "+str(num*i)
+        content+="\n"+str(num)+" x "+str(i)+" = "+str(num*i)
         i=i+1
-
-    f = open("tabla-n.txt", "w")
+    print(content)
+    f = open("tabla-"+str(num)+".txt", "w")
     f.write(content)
     f.close()
+    print("Gracias, feliz dia")
 
 pedir_fruta()
 tabla_de_multiplicar()
